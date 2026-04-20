1d8aee0243f52111a2bf77030f62c32b249bf4bb
ateeqd-dev <ateeqd@gmail.com>
2026-04-20 19:53:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
 
 # convert snowflake dataframe to pandas dataframe
-# pd_df=my_dataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.stop()
 
